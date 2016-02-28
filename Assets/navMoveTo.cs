4e44832cd58f52212fea59ff83075244f9a6f659
alerobledo <alerobledo@gmail.com>
2016-02-28 18:50:56 -0300

    Added Loop for the application

--- Assets/navMoveTo.cs
+++ Assets/navMoveTo.cs
@@ -3,11 +3,29 @@ using System.Collections;
 
 public class navMoveTo : MonoBehaviour {
 
+    private NavMeshAgent agent;
     public Transform goal;
+    private Vector3 originalPosition;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+        originalPosition = agent.transform.position;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance == 0)
+        {
+            if (transform.position.x >= 8f)
+            {
+                agent.destination = originalPosition;
+            }
+            else
+            {
+                agent.destination = goal.position;
+            }
+        }
     }
 }
