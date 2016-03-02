890531b5e69d910ee3920ac22c10e799d3d79c71
alerobledo <alerobledo@gmail.com>
2016-03-02 14:31:20 -0300

    Added offline video playing with a TV assets from assetStore

---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        movie.Play();
+	}
+	
+}
